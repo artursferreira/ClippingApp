cd69d046bb2b0f5d2ff60a28c725bb3423d218b5
artur <artursferreira27@gmail.com>
2021-02-04 20:26:47 -0300

    lesson 6

--- app/src/main/java/com/example/android/clippingexample/MainActivity.kt
+++ app/src/main/java/com/example/android/clippingexample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android.clippingexample
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
@@ -18,4 +20,15 @@ class ClippedView @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    private val path = Path()
+
+
 }
