b93510a31c53ef866a1f840fb19b3e468d27eeeb
artur <artursferreira27@gmail.com>
2021-02-04 21:06:00 -0300

    lesson 20

--- app/src/main/java/com/example/android/clippingexample/MainActivity.kt
+++ app/src/main/java/com/example/android/clippingexample/MainActivity.kt
@@ -215,10 +215,10 @@ class ClippedView @JvmOverloads constructor(
 
     private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
         canvas.save()
-        canvas.translate(columnTwo,rowThree)
+        canvas.translate(columnTwo, rowThree)
         path.rewind()
         path.addRoundRect(
-            rectF,clipRectRight / 4,
+            rectF, clipRectRight / 4,
             clipRectRight / 4, Path.Direction.CCW
         )
         canvas.clipPath(path)
@@ -228,18 +228,42 @@ class ClippedView @JvmOverloads constructor(
 
     private fun drawOutsideClippingExample(canvas: Canvas) {
         canvas.save()
-        canvas.translate(columnOne,rowFour)
-        canvas.clipRect(2 * rectInset,2 * rectInset,
+        canvas.translate(columnOne, rowFour)
+        canvas.clipRect(
+            2 * rectInset, 2 * rectInset,
             clipRectRight - 2 * rectInset,
-            clipRectBottom - 2 * rectInset)
+            clipRectBottom - 2 * rectInset
+        )
         drawClippedRectangle(canvas)
         canvas.restore()
     }
 
     private fun drawTranslatedTextExample(canvas: Canvas) {
+        canvas.save()
+        paint.color = Color.GREEN
+        // Align the RIGHT side of the text with the origin.
+        paint.textAlign = Paint.Align.LEFT
+        // Apply transformation to canvas.
+        canvas.translate(columnTwo, textRow)
+        // Draw text.
+        canvas.drawText(
+            context.getString(R.string.translated),
+            clipRectLeft, clipRectTop, paint
+        )
+        canvas.restore()
     }
 
     private fun drawSkewedTextExample(canvas: Canvas) {
+        canvas.save()
+        paint.color = Color.YELLOW
+        paint.textAlign = Paint.Align.RIGHT
+        // Position text.
+        canvas.translate(columnTwo, textRow)
+        // Apply skew transformation.
+        canvas.skew(0.2f, 0.3f)
+        canvas.drawText(context.getString(R.string.skewed),
+            clipRectLeft, clipRectTop, paint)
+        canvas.restore()
     }
 
     private fun drawQuickRejectExample(canvas: Canvas) {
